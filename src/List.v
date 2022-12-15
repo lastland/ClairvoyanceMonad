643876804da9c5a6b7ada12ba7386f6ed3a8a1bd
Li-yao Xia <lysxia@gmail.com>
2022-12-15 15:19:59 +0000

    Sketch spec for a lazy sort

--- src/List.v
+++ src/List.v
@@ -779,4 +779,51 @@ Proof.
   inversion 1; subst; constructor. inversion H2; constructor; cbn; [ auto | reflexivity ].
 Qed.
 
+Fixpoint selectA_ (l : listA nat) : M (option (T (listA nat) * nat)) :=
+  tick >>
+  match l with
+  | NilA => ret None
+  | ConsA x xs =>
+    forcing x (fun x =>
+    forcing xs (fun xs =>
+    let! o := selectA_ xs in
+    match o with
+    | None => ret (Some (Thunk NilA, x))
+    | Some (ys, y) =>
+      if x <? y then
+        ret (Some (Thunk (ConsA (Thunk y) ys), x))
+      else
+        ret (Some (Thunk (ConsA (Thunk x) ys), y))
+    end))
+  end.
 
+(* Invariant: n = length l. n is the decreasing argument. *)
+Fixpoint selectsortA (n : nat) (l : T (listA nat)) : M (listA nat) :=
+  tick >>
+  let! l := force l in
+  let! o := selectA_ l in
+  match n, o with
+  | S n, Some (ys, y) =>
+    let~ zs := selectsortA n ys in
+    ret (ConsA (Thunk y) zs)
+  | _, _ => ret NilA
+  end.
+
+Parameter selectsort : forall (l : list nat), list nat.
+
+Lemma selectsortA_cost {l n}
+  : n = length l ->
+    forall (d : listA nat), d `is_approx` exact (selectsort l) ->
+    let m := sizeX' 1 d in
+    selectsortA n (exact l) [[ fun sorted cost => d `less_defined` sorted /\ cost <= m * m ]].
+Proof.
+Admitted.
+
+Lemma selectsortA_cost {l n}
+  : n = length l ->
+    forall (d : listA nat), d `is_approx` exact (selectsort l) ->
+    exists (lA : T (listA nat)), lA `is_approx` l /\
+    let m := sizeX' 1 d in
+    selectsortA n lA [[ fun sorted cost => d `less_defined` sorted /\ cost <= m * m ]].
+Proof.
+Admitted.
