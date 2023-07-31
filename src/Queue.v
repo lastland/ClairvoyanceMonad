6b9b6060cfed9f74f1a7f2a150f3bd3a48ff7e30
laisrael-pdx <laisrael@pdx.edu>
2023-07-31 10:19:05 -0700

    Two list queue implementations
    Implemented pure push, pure pop, and demand-model push. Included basic tests and comments with pop WIP code and notes on future proofs.

--- src/Queue.v
+++ src/Queue.v
@@ -24,11 +24,95 @@ Qed.
 
 (* ---------------------- Queue operations ---------------------- *)
 
-Class (*Record?*) two_list_queue (a : Type) : Type := TwoListQueue
+Record two_list_queue (a : Type) : Type := TwoListQueue
 {
   QueueFront : list a;
   QueueBack : list a
 }.
 
-Definition TwoListQueue
-(*Properties of Queue*)
+Definition Push_TwoListQueue (a : Type) (queue : two_list_queue a) (appending : a) : two_list_queue a :=
+  let back := appending :: QueueBack queue in
+  let front := QueueFront queue in
+  TwoListQueue front back.
+
+Definition SwapBack (a : Type) (queue : two_list_queue a) : (two_list_queue a) :=
+  match QueueFront queue with
+  | nil => TwoListQueue (rev (QueueBack queue)) nil
+  | _ => queue
+end.
+
+Definition Pop_TwoListQueue (a : Type) (queue : two_list_queue a) : ((option a) * (two_list_queue a)) :=
+  let new_queue := (SwapBack queue) in
+  match QueueFront new_queue with
+  | nil => (None, TwoListQueue nil nil)
+  | x :: xs => (Some x, TwoListQueue xs (QueueBack new_queue))
+  end.
+
+Record two_list_queueA (a : Type) : Type := TwoListQueueA
+{
+  QueueFrontA : T (listA a);
+  QueueBackA : T (listA a)
+}.
+
+Definition Push_TwoListQueueD (a : Type) (queue : two_list_queue a) (appending : a)
+  (outD : two_list_queueA a) : Tick (T (two_list_queueA a)) :=
+  Tick.tick >>
+  match (QueueBackA outD) with
+  | Undefined => bottom
+  | Thunk xsD => match xsD with
+    | NilA => bottom
+    | ConsA yD ysD => Tick.ret (Thunk (TwoListQueueA (QueueFrontA outD) (exact (ConsA yD ysD))))
+  end
+end.
+
+(** Unfinished Pop_TwoListQueueD implementation **)
+
+(* Definition Pop_TwoListQueueD (a : Type) (queue : two_list_queue a)
+  (outD : (two_list_queueA a) * a) : Tick (T (two_list_queueA a)) :=
+  Tick.tick >>
+  match outD (*Need to access queuefront of outD's first element*) with
+  | (Undefined, _, _) => bottom
+  | (_, Undefined, _) => bottom
+  | (Thunk xsD, Thunk ysD, popped) => match xsD with
+    | NilA => bottom
+    | ConsA yD ysD => Tick.ret (Thunk outD)
+    end
+  end. *)
+
+(** Tests! **)
+
+Compute Pop_TwoListQueue (TwoListQueue [1;2;3] []).
+
+Compute Pop_TwoListQueue (TwoListQueue [1;2;3] [4;5;6]).
+
+Compute Pop_TwoListQueue (TwoListQueue [] [1;2;3]).
+
+Compute Pop_TwoListQueue (TwoListQueue [] []).
+
+Compute Push_TwoListQueue (TwoListQueue [1;2;3] []) 1.
+
+Compute Push_TwoListQueue (TwoListQueue [1;2;3] [4;5;6]) 2.
+
+Compute Push_TwoListQueue (TwoListQueue [] [1;2;3]) 3.
+
+Compute Push_TwoListQueue (TwoListQueue [] []) 4.
+
+Compute Push_TwoListQueueD (TwoListQueue [1;2;3] []) 1 (TwoListQueueA ((exact [1;2;3])) (exact [1])).
+
+Compute Push_TwoListQueueD (TwoListQueue [1;2;3] [4;5;6]) 2 (TwoListQueueA ((exact [1;2;3])) (exact [2;4;5;6])).
+
+Compute Push_TwoListQueueD (TwoListQueue [] [1;2;3]) 3 (TwoListQueueA ((exact [])) (exact [3;1;2;3])).
+
+Compute Push_TwoListQueueD (TwoListQueue [] []) 4 (TwoListQueueA ((exact [])) (exact [4])).
+
+(** Proofs! **)
+
+(*
+
+To prove:
+- Push is constant time; state and prove it.
+- If I finish it, think about best way to prove functional correctness.
+  - Equivalence to pure version?
+    - Prove that the pure version is also correct by showing that it's equal to a *lesser* queue using just one list
+
+*)
