76c2c0f01b40a61a5da9570001a23f06f2246478
laisrael-pdx <laisrael@pdx.edu>
2023-10-02 08:09:59 -0700

    PopD Mostly Done
    Pushing for debugging

--- src/Queue.v
+++ src/Queue.v
@@ -2,7 +2,7 @@
 From Coq Require Import Arith List Psatz Morphisms Relations.
 From Equations Require Import Equations.
 
-From Clairvoyance Require Import Core Approx ApproxM Tick Misc List ListA QueueInterface QueueTrace BankersQueue.
+From Clairvoyance Require Import Core Approx ApproxM Tick Misc List ListA QueueInterface QueueTrace BankersQueue Bool.
 
 #[local] Existing Instance Exact_id | 1.
 #[local] Existing Instance LessDefined_id | 100.
@@ -59,28 +59,94 @@ Definition SwapBack (a : Type) (queue : two_list_queue a) : (two_list_queue a) :
   | _ => queue
 end.
 
-Definition Pop_TwoListQueue (a : Type) (queue : two_list_queue a) : ((option a) * (two_list_queue a)) :=
-  let new_queue := (SwapBack queue) in
-  match QueueFront new_queue with
-  | nil => (None, TwoListQueue nil nil)
-  | x :: xs => (Some x, TwoListQueue xs (QueueBack new_queue))
-  end.
-
-(*Borrowed from Banker's Queue in large part.*)
-
-(*Quoted from there...*)
-
-(* "[mkQueue] uses [rev] and [append], in this order ([append front (rev back)]),
-   so we compute the demand in reverse." *)
 
 (*
 
-Output should be the inputs needed to arrive at outD
+0:
 
-Close read 1989 paper to get a better idea of the mechanical translation
-- Projections vs. pattern matching
+type queue a = (list a * list a)
 
- *)
+pop q = pop_aux (swapback q)
+
+swapback ([], back) = (rev back, [])
+swapback q@(_ :: _, _) = q
+
+pop_aux (x :: front, back) = (Some x, (front, back))
+pop_aux q@([], _) = (None, q)
+
+Find the demand translation of pop_aux...
+
+1:
+
+pop_auxD q outD = ?
+
+2:
+
+(<=* = approximates, either `less_defined` or `is_approx` in Coq
+
+outD <=* pop_aux_q -> pop_aux q outD <= q
+
+outD <=* pop q -> popD q outD <= q
+
+outD <=* swapback q -> swapbackD q outD <= q
+
+3:
+
+pop_auxD q outD = ? <=* q
+  assuming outD <=* pop_aux q
+
+Simplify the assumption until we can fill ? to satisfy ? <=* q
+- Do this by pattern matching on q
+
+4:
+
+pop_auxD (x :: (front, back)) outD = ? <=* (x :: (front, back))
+  assuming outD <=* pop_aux (x (front, back))
+
+pop_auxD ([], back) outD = ? <=* (x :: (front, back))
+  assuming outD <=* pop_aux ([], back)
+
+5:
+
+outD <=* pop_aux (x :: (front, back))
+     <=* (Some x, (front, back))
+
+outD <=* pop_aux q@([], back)
+     <=* (None, q)
+
+6:
+
+pop_auxD (x :: (front, back)) (Some xD, (frontD, backD)) = ? <=* (x :: (front, back))
+  assuming (Some xD, (frontD, backD)) <=* (Some x, (front, back))
+
+pop_auxD ([], back) (None, qD) = ? <=* q@([], back)
+  assuming (None, qD) <=* (None, q)
+
+7:
+
+pop_auxD (x :: (front, back) (Some xD, (frontD, backD))) = ? <=* (x :: (front, back))
+  assuming
+    xD <=* x
+    frontD <=* front
+    backD <=* back
+
+pop_auxD ([], back) (None, qD) = ? <=* q@([], back)
+  assuming
+    qD <=* q
+
+8:
+
+pop_auxD (x :: (front, back)) (Some xD, (frontD, backD)) = (xD :: (frontD, backD)) <=* (x :: (front, back))
+  assuming
+    xD <=* x
+    frontD <=* front
+    backD <=* back
+
+pop_auxD ([], back) (None, qD) = qD <=* q
+  assuming
+    qD <=* q
+
+*)
 
 Definition SwapBackD {a} (queue : two_list_queue a) (outD : (option a * two_list_queueA a)) :
   Tick (T (listA a) * T (listA a)) :=
@@ -101,6 +167,111 @@ Definition SwapBackD {a} (queue : two_list_queue a) (outD : (option a * two_list
   | nil => Tick.ret (QueueFrontA (exact queue), QueueBackA (exact queue))
   end.
 
+Open Scope tick_scope.
+
+Locate ConsD.
+
+Print ConsD.
+
+(*
+
+pop q = pop_aux (swapback q)
+
+swapback ([], back) = (rev back, [])
+swapback q@(_ :: _, _) = q
+
+pop_aux (x :: front, back) = (Some x, (front, back))
+pop_aux q@([], _) = (None, q)
+
+pop_auxD (x :: front, back) (Some xD, (frontD, backD)) = (xD :: frontD, backD) <= (x :: front, back)
+  assuming 
+    xD <= x
+    frontD <= front
+    backD <= back
+
+pop_auxD ([], back) (None, qD) = qD <= queue
+  assuming 
+    qD <= queue
+      queue = ([], back)
+
+outD <= pop_aux queue ->
+pop_auxD queue outD <= queue
+
+*)
+
+Definition pop_aux (a : Type) (queue : two_list_queue a) : (option a * two_list_queue a) :=
+  match QueueFront queue with
+  | x :: xs => (Some x, TwoListQueue xs (QueueBack queue))
+  | nil => (None, queue)
+end.
+
+Check optionA.
+Print optionA.
+
+Definition pop_auxD (a : Type) (queue : two_list_queue a) (outD : (optionA a * two_list_queueA a)) :
+  Tick (T (two_list_queueA a)) :=
+  Tick.tick >>
+  match QueueFront queue with
+  | x :: xs => match outD with
+    | (SomeA xD, TwoListQueueA frontD backD) => Tick.ret (Thunk (TwoListQueueA (Thunk (ConsA xD frontD)) backD))
+    | _ => bottom
+    end
+  | nil => match outD with
+    | (NoneA, qD) => Tick.ret (Thunk qD)
+    | _ => bottom
+  end
+end.
+
+Definition Pop_TwoListQueue (a : Type) (queue : two_list_queue a) : (option a * (two_list_queue a)) :=
+  let new_queue := (SwapBack queue) in
+  pop_aux new_queue.
+(*   match QueueFront new_queue with
+  | nil => (None, TwoListQueue nil nil)
+  | x :: xs => (Some x, TwoListQueue xs (QueueBack new_queue)) *)
+
+Definition Pop_TwoListQueueD (a : Type) (queue : two_list_queue a) (outD : (optionA a * two_list_queueA a)) :
+Tick (T (two_list_queueA a)) :=
+  Tick.tick >>
+  let new_queue := (SwapBack queue) in
+    let+ queueD := pop_auxD new_queue outD in
+      (*Need to put an optionA there for type signature*)
+      SwapBackD queue queueD.
+  (*
+  Why no SwapBackD, again? Don't we need a Tick if it happens?
+  Don't we need to show that SwapBack doesn't make this linear time?
+  *)
+  (* pop_auxD new_queue outD. *)
+(*   match QueueFront new_queue with
+  | nil => Tick.ret (Thunk (TwoListQueueA (Thunk NilA) (Thunk NilA)))
+  | x :: xs => let queueD := SwapBackD queue in 
+    match outD with
+    | (Some xD, TwoListQueueA frontD backD) => pop_auxD queue ((Some (Thunk xD)), (TwoListQueueA frontD backD))
+    | _ => bottom
+    end
+  end. *)
+
+(* Definition Pop_TwoListQueueD (a : Type) (queue : two_list_queue a) (outD : (option a * two_list_queueA a)) :
+  Tick (T (two_list_queueA a)) :=
+  Tick.tick >>
+  match outD with
+  | (None, _) => Tick.ret (Thunk (TwoListQueueA (Thunk NilA) (Thunk NilA)))
+  (* outD can't be known here, should I just write SwapBackA instead? *)
+  (* Or is there a way to convert a TLQA to a TLQ? *)
+  (* Or did I write SwapBackD unhelpfully before? *)
+  | (Some xA, TwoListQueueA frontA backA) => Tick.ret (Thunk (ConsA (Thunk xA) (SwapBackD (TwoListQueueA frontA backA) outD )))
+  | _ => bottom
+  end. *)
+
+(*
+
+Output should be the inputs needed to arrive at outD
+
+Close read 1989 paper to get a better idea of the mechanical translation
+- Projections vs. pattern matching
+
+ *)
+
+(*
 Open Scope tick_scope.
 
 Definition Pop_TwoListQueueD (a : Type) (queue : two_list_queue a) (outD : ((option a)) * (two_list_queueA a)) :
@@ -131,7 +302,7 @@ Definition Pop_TwoListQueueD (a : Type) (queue : two_list_queue a) (outD : ((opt
   | _, (Some xA, queueA) => Tick.ret (Thunk (appendD xA (QueueBackA new_queueA)))
   (*Can't happen - will never have a non-empty list and an empty demand*)
   | _, _ => bottom
-  end.
+  end. *)
 
 (** Tests! **)
 
